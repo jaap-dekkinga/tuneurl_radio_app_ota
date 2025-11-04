54834e476ca401b5170b27120b270bb9a33360da
Jaap Dekkinga <jaap.dekkinga@gmail.com>
2025-11-04 19:08:15 +0000

    Fix: value issue

--- FRadioPlayer/Sources/FRadioPlayer/FRadioMetadataExtractor.swift
+++ FRadioPlayer/Sources/FRadioPlayer/FRadioMetadataExtractor.swift
@@ -17,11 +17,17 @@ struct DefaultMetadataExtractor: FRadioMetadataExtractor {
     func extract(from groups: [AVTimedMetadataGroup]) -> FRadioPlayer.Metadata? {
         guard !groups.isEmpty else { return nil }
         
-        let rawValue = groups.first?.items.first?.value as? String
+        // Use stringValue instead of deprecated `value`
+        let rawValue = groups.first?.items.first?.stringValue
         let rawValueCleaned = cleanRawMetadataIfNeeded(rawValue)
         let parts = rawValueCleaned?.components(separatedBy: " - ")
         
-        return FRadioPlayer.Metadata(artistName: parts?.first, trackName: parts?.last, rawValue: rawValueCleaned, groups: groups)
+        return FRadioPlayer.Metadata(
+            artistName: parts?.first,
+            trackName: parts?.last,
+            rawValue: rawValueCleaned,
+            groups: groups
+        )
     }
     
     private func cleanRawMetadataIfNeeded(_ rawValue: String?) -> String? {
@@ -33,7 +39,12 @@ struct DefaultMetadataExtractor: FRadioMetadataExtractor {
         guard let regex = try? NSRegularExpression(pattern: pattern, options: []) else { return rawValue }
         
         let rawCleaned = NSMutableString(string: rawValue)
-        regex.replaceMatches(in: rawCleaned , options: .reportProgress, range: NSRange(location: 0, length: rawCleaned.length), withTemplate: "")
+        regex.replaceMatches(
+            in: rawCleaned,
+            options: .reportProgress,
+            range: NSRange(location: 0, length: rawCleaned.length),
+            withTemplate: ""
+        )
         
         return rawCleaned as String
     }
