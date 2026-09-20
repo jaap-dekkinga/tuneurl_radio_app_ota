4d091d567024b67ffe3438ac2072cfd88388e29a
Jaap Dekkinga <jaap.dekkinga@gmail.com>
2026-09-20 15:05:49 -0400

    Add timestamp to log messages

--- TuneURLRadio/Helpers/Log.swift
+++ TuneURLRadio/Helpers/Log.swift
@@ -1,23 +1,33 @@
 import Pulse
+import Foundation
 
 struct Log {
     private let label: String
-    
+
+    private static let timestampFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss.SSS"
+        return formatter
+    }()
+
     init(label: String) {
         self.label = label
     }
-    
+
     func write(
         _ message: String,
         level: LoggerStore.Level = .info
     ) {
+        let timestamp = Log.timestampFormatter.string(from: Date())
+        let stamped = "[\(timestamp)] \(message)"
+
 #if DEBUG
-        print(message)
+        print(stamped)
 #endif
         LoggerStore.shared.storeMessage(
             label: label,
             level: level,
-            message: message
+            message: stamped
         )
     }
 }
